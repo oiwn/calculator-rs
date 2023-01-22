340575dbadcf12feffa4590bb14f066f4e8f485f
oiwn <alex@imscraping.ninja>
2023-01-22 07:38:10 +0700

    add unary sign flipping, move calculator logic to separate module

---
+++ src/calculator.rs
@@ -0,0 +1,150 @@
+pub enum Events {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Neg,
+    Number(i64),
+    Eq,
+    Reset,
+    #[allow(dead_code)]
+    Idle,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Tokens {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Number(i64),
+}
+
+pub struct Calculator {
+    ops: Vec<Tokens>,
+    accumulator: i64,
+}
+
+impl Default for Calculator {
+    fn default() -> Self {
+        Self {
+            ops: vec![],
+            accumulator: 0,
+        }
+    }
+}
+
+fn shunting_yard(tokens: Vec<Tokens>) -> Vec<Tokens> {
+    let mut output_queue = vec![];
+    let mut operator_stack = vec![];
+
+    for token in tokens {
+        match token {
+            Tokens::Number(n) => output_queue.push(Tokens::Number(n)),
+            Tokens::Add | Tokens::Sub => {
+                while let Some(top) = operator_stack.last() {
+                    if *top == Tokens::Add || *top == Tokens::Sub {
+                        output_queue.push(operator_stack.pop().unwrap());
+                    } else {
+                        break;
+                    }
+                }
+                operator_stack.push(token);
+            }
+            Tokens::Mul | Tokens::Div => {
+                while let Some(top) = operator_stack.last() {
+                    if *top == Tokens::Mul || *top == Tokens::Div {
+                        output_queue.push(operator_stack.pop().unwrap());
+                    } else {
+                        break;
+                    }
+                }
+                operator_stack.push(token);
+            }
+        }
+    }
+
+    while let Some(op) = operator_stack.pop() {
+        output_queue.push(op);
+    }
+
+    output_queue
+}
+
+impl Calculator {
+    fn calculate(&mut self) -> i64 {
+        println!("Ops: {:?}", self.ops.clone());
+        println!("Algo: {:?}", shunting_yard(self.ops.clone()));
+        let mut stack = vec![];
+
+        for token in shunting_yard(self.ops.clone()) {
+            match token {
+                Tokens::Number(n) => stack.push(n),
+                Tokens::Add => {
+                    let y = stack.pop().unwrap();
+                    let x = stack.pop().unwrap();
+                    stack.push(x + y);
+                }
+                Tokens::Sub => {
+                    let y = stack.pop().unwrap();
+                    let x = stack.pop().unwrap();
+                    stack.push(x - y);
+                }
+                Tokens::Mul => {
+                    let y = stack.pop().unwrap();
+                    let x = stack.pop().unwrap();
+                    stack.push(x * y);
+                }
+                Tokens::Div => {
+                    let y = stack.pop().unwrap();
+                    let x = stack.pop().unwrap();
+                    stack.push(x / y);
+                }
+            }
+        }
+        stack.pop().unwrap()
+    }
+
+    pub fn display(&self) -> String {
+        self.accumulator.to_string()
+    }
+
+    pub fn dispatch(&mut self, event: Events) {
+        match event {
+            Events::Idle => {}
+            Events::Eq => {
+                // here will be complex logic
+                self.ops.push(Tokens::Number(self.accumulator));
+                self.accumulator = self.calculate();
+                self.ops.clear();
+            }
+            Events::Reset => {
+                self.ops.clear();
+                self.accumulator = 0;
+            }
+            Events::Neg => {
+                self.accumulator *= -1;
+            }
+            Events::Number(num) => {
+                if self.accumulator <= 999_999_999_9 {
+                    self.accumulator *= 10;
+                    self.accumulator += num as i64;
+                }
+            }
+            op @ (Events::Add | Events::Sub | Events::Mul | Events::Div) => {
+                // operation first
+                let op_token: Option<Tokens> = match op {
+                    Events::Add => Some(Tokens::Add),
+                    Events::Sub => Some(Tokens::Sub),
+                    Events::Mul => Some(Tokens::Mul),
+                    Events::Div => Some(Tokens::Div),
+                    _ => None,
+                };
+
+                self.ops.push(Tokens::Number(self.accumulator));
+                self.ops.push(op_token.unwrap());
+                self.accumulator = 0
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@
 use eframe::egui;
 use egui::{FontFamily, FontId, TextStyle};
 
+mod calculator;
+
+use calculator::{Calculator, Events};
+
 fn main() {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
@@ -16,148 +20,6 @@ fn main() {
         options,
         Box::new(|_cc| Box::new(Calculator::default())),
     );
-}
-
-enum Events {
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Number(i64),
-    Eq,
-    Reset,
-    Idle,
-}
-
-#[derive(Debug, PartialEq, Clone)]
-enum Tokens {
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Number(i64),
-}
-
-struct Calculator {
-    ops: Vec<Tokens>,
-    accumulator: i64,
-}
-
-impl Default for Calculator {
-    fn default() -> Self {
-        Self {
-            ops: vec![],
-            accumulator: 0,
-        }
-    }
-}
-
-fn shunting_yard(tokens: Vec<Tokens>) -> Vec<Tokens> {
-    let mut output_queue = vec![];
-    let mut operator_stack = vec![];
-
-    for token in tokens {
-        match token {
-            Tokens::Number(n) => output_queue.push(Tokens::Number(n)),
-            Tokens::Add | Tokens::Sub => {
-                while let Some(top) = operator_stack.last() {
-                    if *top == Tokens::Add || *top == Tokens::Sub {
-                        output_queue.push(operator_stack.pop().unwrap());
-                    } else {
-                        break;
-                    }
-                }
-                operator_stack.push(token);
-            }
-            Tokens::Mul | Tokens::Div => {
-                while let Some(top) = operator_stack.last() {
-                    if *top == Tokens::Mul || *top == Tokens::Div {
-                        output_queue.push(operator_stack.pop().unwrap());
-                    } else {
-                        break;
-                    }
-                }
-                operator_stack.push(token);
-            }
-        }
-    }
-
-    while let Some(op) = operator_stack.pop() {
-        output_queue.push(op);
-    }
-
-    output_queue
-}
-
-impl Calculator {
-    fn calculate(&mut self) -> i64 {
-        println!("Ops: {:?}", self.ops.clone());
-        println!("Algo: {:?}", shunting_yard(self.ops.clone()));
-        let mut stack = vec![];
-
-        for token in shunting_yard(self.ops.clone()) {
-            match token {
-                Tokens::Number(n) => stack.push(n),
-                Tokens::Add => {
-                    let y = stack.pop().unwrap();
-                    let x = stack.pop().unwrap();
-                    stack.push(x + y);
-                }
-                Tokens::Sub => {
-                    let y = stack.pop().unwrap();
-                    let x = stack.pop().unwrap();
-                    stack.push(x - y);
-                }
-                Tokens::Mul => {
-                    let y = stack.pop().unwrap();
-                    let x = stack.pop().unwrap();
-                    stack.push(x * y);
-                }
-                Tokens::Div => {
-                    let y = stack.pop().unwrap();
-                    let x = stack.pop().unwrap();
-                    stack.push(x / y);
-                }
-            }
-        }
-        stack.pop().unwrap()
-    }
-
-    fn dispatch(&mut self, event: Events) {
-        match event {
-            Events::Idle => {}
-            Events::Eq => {
-                // here will be complex logic
-                self.ops.push(Tokens::Number(self.accumulator));
-                self.accumulator = self.calculate();
-                self.ops.clear();
-            }
-            Events::Reset => {
-                self.ops.clear();
-                self.accumulator = 0;
-            }
-            Events::Number(num) => {
-                if self.accumulator <= 999_999_999_9 {
-                    self.accumulator *= 10;
-                    self.accumulator += num as i64;
-                }
-            }
-            op @ (Events::Add | Events::Sub | Events::Mul | Events::Div) => {
-                // operation first
-                let op_token: Option<Tokens> = match op {
-                    Events::Add => Some(Tokens::Add),
-                    Events::Sub => Some(Tokens::Sub),
-                    Events::Mul => Some(Tokens::Mul),
-                    Events::Div => Some(Tokens::Div),
-                    _ => None,
-                };
-
-                self.ops.push(Tokens::Number(self.accumulator));
-                self.ops.push(op_token.unwrap());
-                self.accumulator = 0
-            }
-        }
-    }
 }
 
 fn configure_text_styles(ctx: &egui::Context) {
@@ -182,7 +44,7 @@ impl eframe::App for Calculator {
             configure_text_styles(ctx);
 
             if ui
-                .add_enabled(false, egui::Button::new(self.accumulator.to_string()))
+                .add_enabled(false, egui::Button::new(self.display()))
                 .clicked()
             {
                 unreachable!();
@@ -193,7 +55,7 @@ impl eframe::App for Calculator {
                     self.dispatch(Events::Reset);
                 }
                 if ui.button("±").clicked() {
-                    self.dispatch(Events::Idle);
+                    self.dispatch(Events::Neg);
                 }
                 let _ = ui.button("(");
                 let _ = ui.button(")");
